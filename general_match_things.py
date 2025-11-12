c1fe7b9e8ea046b49aa949be44596a4694345465
GosProgrammers <girlsofsteelrobotics@gmail.com>
2025-11-11 20:27:29 -0500

    average pattern plots

--- general_match_things.py
+++ general_match_things.py
@@ -9,13 +9,43 @@ from data_container import df
 @module.ui
 def general_match_ui():
     return ui.page_fluid(
-        output_widget("team_v_total_points")
+        output_widget("team_v_total_points"),
+        output_widget("avg_auto_pattern_count"),
+        output_widget("avg_teleop_pattern_count"),
+        output_widget("avg_combined_pattern_count")
     )
 @module.server
 def general_match_server(input,output,session):
+
+    def get_teams_in_match():
+        all_teams = [3333, 6666, 11111, 4444]
+        new_df = df.loc[df["Team Number"].isin(all_teams)]
+        return new_df
+
     @render_widget
     def team_v_total_points():
         all_teams = [3333, 6666, 11111, 4444]
         new_df = df.loc[df["Team Number"].isin(all_teams)]
         fig = px.box(new_df, x="Team Number", y="Total Points Scored")
         return fig
+
+    @render_widget
+    def avg_auto_pattern_count():
+        avg_team = get_teams_in_match().groupby("Team Number").mean(numeric_only=True)
+        fig_auto_pattern_count = px.bar(avg_team, y="Pattern Correct(Auto)",
+                                        title="Average Pattern Correct by Team in Autonomous")
+        return fig_auto_pattern_count
+
+    @render_widget
+    def avg_teleop_pattern_count():
+        avg_team = get_teams_in_match().groupby("Team Number").mean(numeric_only=True)
+        fig_teleop_pattern_count = px.bar(avg_team, y = "Pattern Correct(Teleop)",
+                                          title = "Average Pattern Correct by Team in Teleop")
+        return fig_teleop_pattern_count
+
+    @render_widget
+    def avg_combined_pattern_count():
+        avg_team = get_teams_in_match().groupby("Team Number").mean(numeric_only=True)
+        fig_general_pattern_count = px.bar(avg_team, y=["Pattern Correct(Auto)", "Pattern Correct(Teleop)"],
+                                           title="Average Pattern Correct by Team in Teleop and Auto")
+        return fig_general_pattern_count
