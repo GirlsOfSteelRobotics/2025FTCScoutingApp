12bf00372561ecd37d6a2d759f347c3e0873a463
Divya Thirumala <divyathirumala@Parthas-MacBook-Air.local>
2025-11-11 20:35:24 -0500

    coded classified v overflow v depot graphs for number and points

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ jupyter lab
 
 ## Running Shiny App
 shiny run app.py --reload
+
+### Colors: warm: de9c63, b54405, a46c54, 852c04 cool: 194f55, 54808e, 243454, 8c746c
--- general_match_things.py
+++ general_match_things.py
@@ -17,5 +17,20 @@ def general_match_server(input,output,session):
     def team_v_total_points():
         all_teams = [3333, 6666, 11111, 4444]
         new_df = df.loc[df["Team Number"].isin(all_teams)]
+        # add team thing
         fig = px.box(new_df, x="Team Number", y="Total Points Scored")
+        return fig
+    @render_widget
+    def classified_v_overflow_v_depot_scored():
+        # add colors, figure out the line separation thing, team thing
+        all_teams = [3333, 6666, 11111, 4444]
+        new_df = df.loc[df["Team Number"].isin(all_teams)]
+        fig = px.bar(new_df, x="Team Number", y=["Classifier Scored(Teleop)", "Overflow Scored(Teleop)", "Depot Scored(Teleop)"], title="Classifier v. Overflow v. Depot Scored(Teleop)")
+        return fig
+    @render_widget
+    def classified_v_overflow_v_depot_points():
+        # add colors, figure out the line separation thing, team thing
+        fig = px.bar(new_df, x="Team Number",
+                     y=["Classifier Scored POINTS(Teleop)", "Overflow Scored POINTS(Teleop)", "Depot Scored(Teleop)"],
+                     title="Classifier v. Overflow v. Depot Scored POINTS(Teleop)")
         return fig
